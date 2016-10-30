417729b56dd0f6df71886db219d65bf603ed5969
Ibrahim Kteish <infinitybob@gmail.com>
2016-10-30 14:44:20 +0200

    Initial Commit

---
+++ RemoteContent/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RemoteContent
+//
+//  Created by Ibrahim Kteish on 10/30/16.
+//  Copyright © 2016 Ibrahim Kteish. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
